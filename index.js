d38f69b8901df8869d0a1a172bbe33679cc0ee66
Ralph Schindler <ralph@ralphschindler.com>
2018-02-15 13:24:54 -0600

    Update manifest, commit to disk

--- index.js
+++ index.js
@@ -7,29 +7,32 @@ class LaravelMixFilenameVersioning {
       const newAssets = {};
 
       Object.keys(stats.compilation.assets).forEach(assetName => {
-          let originalAssetNameParts = path.parse(assetName);
-          let newAssetFile = new File(path.join(Config.publicPath, assetName));
-          let newAssetFileName = newAssetFile.segments.name + '.' + newAssetFile.version() + newAssetFile.segments.ext;
+        let originalAssetNameParts = path.parse(assetName);
+        let newAssetFile = new File(path.join(Config.publicPath, assetName));
+        let newAssetFileName = newAssetFile.segments.name + '.' + newAssetFile.version() + newAssetFile.segments.ext;
 
-          newAssetFile.rename(newAssetFileName);
+        newAssetFile.rename(newAssetFileName);
 
-          let newAssetKeyName = path.join(originalAssetNameParts.dir, newAssetFileName);
-          let newAssetFullFileName = path.join(newAssetFile.segments.base, newAssetFileName);
-          newAssets[newAssetKeyName] = stats.compilation.assets[assetName];
+        let newAssetKeyName = path.join(originalAssetNameParts.dir, newAssetFileName);
+        let newAssetFullFileName = path.join(newAssetFile.segments.base, newAssetFileName);
+        newAssets[newAssetKeyName] = stats.compilation.assets[assetName];
 
-          if (newAssets[newAssetKeyName].hasOwnProperty('existsAt')) {
-            newAssets[newAssetKeyName].existsAt = newAssetFullFileName;
-          }
-          if (newAssets[newAssetKeyName].hasOwnProperty('absolutePath')) {
-            newAssets[newAssetKeyName].absolutePath = newAssetFullFileName;
-          }
+        if (newAssets[newAssetKeyName].hasOwnProperty('existsAt')) {
+          newAssets[newAssetKeyName].existsAt = newAssetFullFileName;
+        }
+        if (newAssets[newAssetKeyName].hasOwnProperty('absolutePath')) {
+          newAssets[newAssetKeyName].absolutePath = newAssetFullFileName;
+        }
 
-          // this is only a fix for the incorrect asset binding in CustomTaskPlugins.js
-          newAssets[newAssetKeyName].size = function (assetAbsolutePath) {
-            return new File(assetAbsolutePath).size();
-          }.bind(null, newAssetFullFileName);
+        // this is only a fix for the incorrect asset binding in CustomTaskPlugins.js
+        newAssets[newAssetKeyName].size = function (assetAbsolutePath) {
+          return new File(assetAbsolutePath).size();
+        }.bind(null, newAssetFullFileName);
+
+        Mix.manifest.manifest[assetName] = newAssetKeyName;
       });
 
+      Mix.manifest.refresh();
       stats.compilation.assets = newAssets;
     });
   }
